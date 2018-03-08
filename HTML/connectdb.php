f14a4d10f3660a53b53ff8f6698a21d7a8d5bc44
timi2000 <tim_widmer@gmx.ch>
2018-03-08 10:25:40 +0100

    datenbankverbindung hergestellt

--- HTML/connectdb.php
+++ HTML/connectdb.php
@@ -5,9 +5,14 @@
  * Date: 07.03.18
  * Time: 09:03
  */
-$con = mysqli_connect("127.0.0.1","root","root", "mydb", "3306");
+/*$con = mysqli_connect("127.0.0.1","root","root", "mydb", "3306");
 if (mysqli_connect_errno())
 {
     echo "failed to conect to MySQL: ".mysqli_connect_error();
 }
+
+$frage1 = $_POST["frage1"];
+$frage2 = $_POST["frage2"];
+echo($frage1. "  " .$frage2);
+*/
 ?>
--- HTML/umfrageAuswerten.php
+++ HTML/umfrageAuswerten.php
@@ -65,16 +65,46 @@ if (mysqli_connect_errno())
 {
     echo "failed to conect to MySQL: ".mysqli_connect_error();
 }
-$eintragen = mysqli_query($mydb);
+$Bewertungid =  0;
+$lastid =
+mysqli
+mysqli_query($con,"SELECT idBewertung,Frage1, Frage2, Frage3, Frage4, Frage5, Frage6, Frage7, Frage8, Frage9, Frage10, Frage11, Frage12, Frage13, Frage14, Frage15 FROM Bewertung");
+mysqli_query($con,"INSERT INTO Bewertung (idBewertung, Frage1, Frage2, Frage3, Frage4, Frage5, Frage6, Frage7, Frage8, Frage9, Frage10, Frage11, Frage12, Frage13, Frage14, Frage15) 
+Values ($Bewertungid + 3, $frage1, $frage2, $frage3, $frage4, $frage5, $frage6, $frage7, $frage8, $frage9, $frage10, $frage11, $frage12, $frage13, $frage14, $frage15)");
+//$lastid =  mysqli_insert_id($con);
+mysqli_close($con);
 
 
-$eintragen = mysqli_query($db, "INSERT INTO Bewertung (idBewertung, urlname, name, banner, beschreibung) VALUES ('$url', '$hpname', '$name', '$banner', '$beschreibung')");
+
+/*mysqli_query($con,"SELECT Frage1, Frage2 from Bewertung");
+mysqli_query($con, "INSERT INTO Bewertung (Frage1, Frage2) Values ($frage1, $frage2)");
+
+*/
+
+/*if (mysqli_query($con)) {
+    $last_id = mysqli_insert_id($con);
+    echo "New record created successfully. Last inserted ID is: " . $last_id;
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($con);
+}*/
 
 
+/*$sql = "INSERT INTO Bewertung (Frage1, Frage2, Frage3, Frage4, Frage5,Frage6, Frage7, Frage8, Frage9, Frage10, Frage11, Frage12, Frage13, Frage14, Frage15)
+VALUES ( $frage1, $frage2, $frage3, $frage4, $frage5, $frage6, $frage7, $frage8,$frage9, $frage10, $frage11, $frage12, $frage13, $frage14, $frage15)";
+$sqlResult = @mysqli_query($sql);
+*/
+/*$sql = "INSERT INTO Bewertung (Frage1, Frage2, Frage3)
+VALUES ($frage1, $frage2, $frage3)";
 
+$result = @mysqli_query($sql);
+if (!$result) {
+    $message  = 'Ungültige Abfrage: ' . mysqli_error() . "\n";
+    $message .= 'Gesamte Abfrage: ' . $sql;
+    die($message);
+}*/
 // Kommentar in Datenbanktabelle 'comments' schreiben:
-$sql = "insert into Bewertung";
-$sql .= "(idBewertung, frageNR, FrageWert) values (";
+/*$sql = "insert into Bewertung";
+$sql .= "(idBewertung, frageNR, FrageWert) values (";s
 $sql .= $articleid.", ";
 $sql .= $_POST['frage'].", ";
 $sql .= $_POST['mail'].", ";
@@ -82,7 +112,14 @@ $sql .= $_POST['text'].", ";
 $sql .= $timestamp.")";
 $sqlResult = @mysql_query($sql, $dbh);
 if(!$sqlResult) // Datensatz konnte nicht gespeichert werden:
-    $errorMessage .= '<div class="error">Kommentar 
+    $errorMessage .= '<div class="error">Kommentar
             konnte nicht gespeichert werden</div>';
+*/
+/*$statement = $con->prepare("INSERT INTO Bewertung (FrageNR, FrageWert, Frage1, Frage2, Frage3, Frage4, Frage5, Frage6, Frage7, Frage8, Frage9, Frage10, Frage11, Frage12, Frage13, Frage14, Frage15)
+VALUES ('1', '1', $frage1, $frage2, $frage3, $frage4, $frage5, $frage6, $frage7, $frage8, $frage9, $frage10,
+$frage11, $frage12, $frage13, $frage14, $frage15);
+
+
+ $statement->execute(array( 'FrageNr' => '1',' FrageWert' => $frage1, 'Frage1' => $frage1,    'email' => 'info@php-einfach.de', 'vorname' => 'Klaus', 'nachname' => 'Neumann'));
 */
 ?>
